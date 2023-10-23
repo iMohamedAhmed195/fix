d38166637d0867f0930253609e194f669aeda72f
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-10-23 12:18:19 +0300

    added login model & entity

--- lib/features/login_feature/data/models/login_model.dart
+++ lib/features/login_feature/data/models/login_model.dart
@@ -1,1 +1,33 @@
+import 'package:fix/features/login_feature/domain/entities/login_entity.dart';
 
+class LoginModel extends LoginEntity {
+  final String message;
+  final UserData? userData;
+
+  const LoginModel({
+    required status,
+    required token,
+    required this.message,
+    required this.userData,
+  });
+
+  factory LoginModel.fromJson(Map<String, dynamic> json) => LoginModel(
+      message: json['message'],
+      userData: json['data'] != null ? UserData.fromJson(json['data']) : null,
+      status: json['status'],
+      token: json['token']);
+}
+
+class UserData extends LoginEntity {
+  const UserData({
+    isPaid,
+    name,
+    photo,
+    role,
+  });
+  factory UserData.fromJson(Map<String, dynamic> json) => UserData(
+      name: json['name'],
+      photo: json['photo'],
+      isPaid: json['isPaid'],
+      role: json['role']);
+}
--- lib/features/login_feature/domain/entities/login_entity.dart
+++ lib/features/login_feature/domain/entities/login_entity.dart
@@ -1,16 +1,16 @@
 import 'package:equatable/equatable.dart';
 
 class LoginEntity extends Equatable{
-  final bool status ;
-  final bool isPaid ;
-  final String name ;
-  final String photo ;
-  final String role ;
-  final String token ;
+  final bool? status ;
+  final bool? isPaid ;
+  final String? name ;
+  final String? photo ;
+  final String? role ;
+  final String? token ;
 
 
   const LoginEntity(
-      this.status, this.isPaid, this.name, this.photo, this.role, this.token);
+      [this.status, this.isPaid, this.name, this.photo, this.role, this.token]);
 
   @override
   List<Object?> get props => [
