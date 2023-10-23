c1fa3adb712612514014c564764289c1e8ffbcee
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-10-23 20:04:04 +0300

    added component

---
+++ lib/core/utils/widget/component.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class asd extends StatelessWidget {
+  const asd({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
