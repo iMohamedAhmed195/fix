1831825daf1f3da15f910be8ddc271f3e6d8e506
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-11-09 15:48:01 +0200

    EDIT COMPONENT

---
+++ lib/core/utils/go_routes/route_names.dart
@@ -0,0 +1,33 @@
+import 'package:go_router/go_router.dart';
+
+
+
+abstract class AppRouter {
+  static const kOnBoarding = '/kOnBoarding';
+  static const kLogin = '/Login';
+  static const kRegister = '/Register';
+  static const kHome = '/Home';
+  static const kBookDetails = '/BookDetails';
+
+  static final router = GoRouter(
+    routes: [
+      // GoRoute(
+      //   path: '/',
+      //   builder: (context, state) =>   const SplashView(),
+      // ),
+      // GoRoute(
+      //   path: kOnBoarding,
+      //   builder: (context, state) =>   const OnBoardingScreen(),
+      // ),
+      // GoRoute(
+      //   path: kLogin,
+      //   builder: (context, state) =>   const LoginView(),
+      // ),
+      // GoRoute(
+      //   path: kRegister,
+      //   builder: (context, state) =>   const RegisterView(),
+      // ),
+
+    ],
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:fix/core/services/bloc_observer.dart';
 import 'package:fix/core/services/dio_helper.dart';
 import 'package:fix/core/services/services_locator.dart';
-
+import 'package:fix/core/utils/go_routes/route_names.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/services/bloc_observer.dart';
 
 void main() {
   ServiceLocator().init();
@@ -18,8 +19,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      // home: LoginView(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_,child){
+        return MaterialApp.router(
+          routerConfig: AppRouter.router,
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
