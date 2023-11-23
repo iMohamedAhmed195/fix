2160911bdca8f46335affab04fb390b837a531fe
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-11-23 11:34:34 +0200

    creating verification features

---
+++ lib/features/verification_features/forget_password_feature/presentation/view/forget_password_view.dart
@@ -0,0 +1,11 @@
+import 'package:fix/features/verification_features/forget_password_feature/presentation/view/widget/forget_password_view_body.dart';
+import 'package:flutter/material.dart';
+
+class ForgetPasswordView extends StatelessWidget {
+  const ForgetPasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ForgetPasswordViewBody();
+  }
+}
---
+++ lib/features/verification_features/forget_password_feature/presentation/view/widget/forget_password_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ForgetPasswordViewBody extends StatelessWidget {
+  const ForgetPasswordViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+
+      ],
+    );
+  }
+}
