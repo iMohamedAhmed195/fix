3d90e1de4aaa548735b7dc99a36bda1e1e2d203f
Mahmoud Darwish <mahmoudsaaddarwish1@gmail.com>
2023-12-18 17:36:11 +0200

    done register

--- lib/core/widget_components/custom_text_form_field.dart
+++ lib/core/widget_components/custom_text_form_field.dart
@@ -1,5 +1,6 @@
 import 'package:fix/core/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomTextFormField extends StatelessWidget {
@@ -10,7 +11,10 @@ class CustomTextFormField extends StatelessWidget {
       this.function,
       required this.controller,
       required this.obSecure,
-      required this.inputType,this.iconSuffix,this.sufFunction});
+      required this.inputType,
+      this.iconSuffix,
+      this.sufFunction,
+      this.formatList});
 
   final String hintText;
   final IconData iconPrefix;
@@ -20,11 +24,13 @@ class CustomTextFormField extends StatelessWidget {
   final TextEditingController controller;
   final bool obSecure;
   final TextInputType inputType;
+  final List<TextInputFormatter>? formatList;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 20.0.w,vertical: 15.h),
+      padding: EdgeInsets.symmetric(horizontal: 20.0.w, vertical: 15.h),
       child: TextFormField(
+        inputFormatters: formatList,
         validator: function,
         decoration: InputDecoration(
             label: Text(
